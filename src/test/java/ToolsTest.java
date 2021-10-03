d17bf45e0a5b749d095ab67ee77bec2942110f99
dartaxis <filatyev.a.a@gmail.com>
2021-10-03 13:42:39 +0300

    изменение размера формы

--- src/test/java/ToolsTest.java
+++ src/test/java/ToolsTest.java
@@ -2,6 +2,7 @@ import org.junit.jupiter.api.Test;
 import tools.Game;
 
 import static org.junit.jupiter.api.Assertions.assertArrayEquals;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class ToolsTest {
     @Test
@@ -11,5 +12,11 @@ public class ToolsTest {
         assertArrayEquals(Game.guessing(1234, 1243), new Integer[]{2, 2});
         assertArrayEquals(Game.guessing(1234, 5678), new Integer[]{0, 0});
         assertArrayEquals(Game.guessing(1234, 5643), new Integer[]{0, 2});
+    }
+
+    @Test
+    public void testGetMainMap() {
+        assertEquals(Game.getMainMap().size(),5040);
+
     }
 }
